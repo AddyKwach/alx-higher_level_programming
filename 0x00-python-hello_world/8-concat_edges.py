ade70f557b24e66dc4d0026c12149cb47cf743b5
AddyKwach <adriankwach@gmail.com>
2024-01-02 18:01:41 +0300

    new

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 my_str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{my_str[38:67]} {my_str[107:112]} {my_str[:6]}\n")
 
+print(f"{my_str[38:66]}{my_str[106:111]} {my_str[0:6]}\n")
