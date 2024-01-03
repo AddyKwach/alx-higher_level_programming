fb4eafc7eeae979f9e7b35c1b869a7363c9e9c1b
AddyKwach <adriankwach@gmail.com>
2024-01-03 14:14:06 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python3
-
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-# Test the function
-if __name__ == "__main__":
-    islower = __import__('7-islower').islower
-
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
+    if c == c.lower():
+        return True
+    else:
+        return False
