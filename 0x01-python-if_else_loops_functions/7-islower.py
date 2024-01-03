af78c3effe8fc24b26e27f6eb201ff340de1f27c
AddyKwach <adriankwach@gmail.com>
2024-01-03 14:36:52 +0300

    Update 7-islower.py
    original islower script patch

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-    try:
-        return c == c.lower() and c.isalpha()
-    except AttributeError;
-         raise TypeError("Input must be a string")
+    if c == c.lower():
+        return True
+    else:
+        return False
 
